5edd33e104dff52212f134c30bcec7c0db75f6a2
Vinicius <viniciussoares852@gmail.com>
2022-04-05 08:29:08 -0300

    refatorando projeto inicial expo padrao

--- src/scribblenotes/App.js
+++ src/scribblenotes/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import HomePage from './src/pages/HomePage'
-
-const App = () => {
-
+export default function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
- 
-export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

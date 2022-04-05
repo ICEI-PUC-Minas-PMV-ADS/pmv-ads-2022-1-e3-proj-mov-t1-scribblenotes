83cce1d77138feb105f4fa54055f2abaa0245f89
Vinicius <viniciussoares852@gmail.com>
2022-04-04 22:52:03 -0300

    installing pkgs

--- src/scribblenotes/App.js
+++ src/scribblenotes/App.js
@@ -1,30 +1,15 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Tasks from './src/components/Tasks'
-import AppLoading from 'expo-app-loading';
-import Home from './src/screens/Home'
-import { useFonts, Roboto_400Regular, Roboto_500Medium ,Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { DARK_THEME } from './src/styles/themes/themes'
-import { ThemeProvider } from 'styled-components';
+import { View } from 'react-native'
 
-export default function App() {
-  let [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
-  });
+import HomePage from './src/pages/HomePage'
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+const App = () => {
 
   return (
-    <ThemeProvider theme={DARK_THEME}>
-      <Home>
-        <Tasks />
-      </Home>
-    </ThemeProvider>
+    <>
+      <HomePage />
+    </>
   );
 }
  
+export default App
---
+++ src/scribblenotes/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+
+const HomePage = () => {
+  return [
+    <View>
+      Home page
+    </View>
+  ]
+}
+
+export default HomePage
